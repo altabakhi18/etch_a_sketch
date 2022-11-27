ad66cb7353de42168909333fef0c6fb085773ad0
altabakhi18 <altabakhi18@gmail.com>
2022-11-26 19:24:37 -0600

    outline of project done

---
+++ sketch.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector('#container');
+
+for(let i = 0; i < 16; i++){
+
+const divs = document.createElement('div');
+divs.classList.add('one');
+grid.appendChild(divs);
+
+}
