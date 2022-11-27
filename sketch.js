894c9d42a4745a7ab093101b8202c132b5999e91
altabakhi18 <altabakhi18@gmail.com>
2022-11-26 22:12:11 -0600

    finished adding the sketch and the gird button. The grid needs tweaking to get the grid a perfect square.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,50 @@
 const grid = document.querySelector('#container');
+let msg = 0;
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 16 * 16; i++){
 
-const divs = document.createElement('div');
-divs.classList.add('one');
-grid.appendChild(divs);
+    const divs = document.createElement('div');
+    divs.classList.add('one');
+    divs.style.cssText = "width: 40px; height: 40px; border: 1px solid black;"
+    divs.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    });
 
-}
+    grid.appendChild(divs);
+
+    }
+
+    
+
+    function changeGrid(){
+         msg = prompt("Enter a number 1 - 100");
+
+        if(msg > 100){
+            alert("Error! Number must be lower than 100");
+        }
+        else {
+            removeGrid(grid);
+
+        for(let i = 0; i < msg * msg; i++){
+            const divs = document.createElement('div');
+            divs.classList.add('one');
+            divs.style.cssText = "width: 40px; height: 40px; border: 1px solid black;"
+            divs.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        });
+
+            grid.appendChild(divs);
+
+        }
+    }
+    }
+
+    function removeGrid(node){
+        while(node.firstChild){
+            node.removeChild(node.firstChild);
+        }
+    }
+
+
+
+
